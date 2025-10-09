d8d1ff1e370598de8bb4b01487f09c00b050b806
OmgbaKevin <kevinomgba1@gmail.com>
2025-10-09 10:33:49 +0100

    adapted all logo's

--- src/Components/Pages/Auth/LoginForm.jsx
+++ src/Components/Pages/Auth/LoginForm.jsx
@@ -3,15 +3,15 @@ import { Btn, H4, P, Image } from '../../../AbstractElements';
 import { Form, FormGroup, Input, Label } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import { useNavigate } from "react-router-dom";
-import {  toast } from "react-toastify";
+import { toast } from "react-toastify";
 import CustomizerContext from "../../../_helper/Customizer"
 import { loginUser } from '../../../reduxtool/Auth';
 import { useDispatch } from 'react-redux';
 import { PermissionContext } from 'react-permission-role';
 
 
-import logoWhite from '../../../assets/images/logo/logo.png';
-import logoDark from '../../../assets/images/logo/logo_dark.png';
+import logoWhite from '../../../assets/images/logo/logo.jpg';
+import logoDark from '../../../assets/images/logo/logo_dark.jpg';
 
 
 const LoginForm = ({ logoClassMain }) => {
@@ -48,7 +48,7 @@ const LoginForm = ({ logoClassMain }) => {
           target = `${process.env.PUBLIC_URL}/Pages/FeicomPages/HomePage/Page1/${layoutURL}`;
         }
 
-        navigate(target, {replace: true});
+        navigate(target, { replace: true });
 
       } else if (loginUser.rejected.match(user)) {
         const errorMessage = typeof user.payload === 'string'
@@ -65,38 +65,48 @@ const LoginForm = ({ logoClassMain }) => {
   return (
     <Fragment>
       <div className='login-card'>
-          <div>
-            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
-              <Link className={`logo ${logoClassMain ? logoClassMain : ''}`} to={process.env.PUBLIC_URL}>
-                <Image attrImage={{ className: 'img-fluid for-light', src: logoWhite, alt: 'looginpage', style: { maxWidth: '120px', height: 'auto' } }} />
-                <Image attrImage={{ className: 'img-fluid for-dark', src: logoDark, alt: 'looginpage', style: { maxWidth: '120px', height: 'auto' } }} />
-              </Link>
-            </div>
+        <div>
+          <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px' }}>
+            <Link className={`logo ${logoClassMain ? logoClassMain : ''}`} to={process.env.PUBLIC_URL}>
+              <Image attrImage={{
+                className: 'img-fluid for-light',
+                src: logoWhite,
+                alt: 'logo',
+                style: { maxWidth: '120px', height: 'auto', backgroundColor: 'transparent' }
+              }} />
+              <Image attrImage={{
+                className: 'img-fluid for-dark',
+                src: logoDark,
+                alt: 'logo',
+                style: { maxWidth: '120px', height: 'auto', backgroundColor: 'transparent' }
+              }} />
+            </Link>
+          </div>
 
-            <div className='login-main'>
-              <Form className='theme-form login-form' onSubmit={loginAuth}>
-                <H4>Login in to account</H4>
-                <P>Enter your username & password to login</P>
-                <FormGroup>
-                  <Label className='col-form-label m-0'>Username <i className="icofont icofont-businessman"></i></Label>
-                  <Input className='form-control' type='text' placeholder='User_name' onChange={(e) => setUsername(e.target.value)} value={username}/>
-                </FormGroup>
-                <FormGroup className='position-relative'>
-                  <Label className='col-form-label m-0'>Password <i className="icofont icofont-ui-fire-wall"></i></Label>
-                  <div className='position-relative'>
-                    <Input className='form-control' type={togglePassword ? 'text' : 'password'} name='login[password]' required placeholder='*********' onChange={(e) => setPassword(e.target.value)} value={password}/>
-                    <div className='show-hide' onClick={() => setTogglePassword(!togglePassword)}>
-                      <span className={togglePassword ? '' : 'show'}></span>
-                    </div>
+          <div className='login-main'>
+            <Form className='theme-form login-form' onSubmit={loginAuth}>
+              <H4>Login in to account</H4>
+              <P>Enter your username & password to login</P>
+              <FormGroup>
+                <Label className='col-form-label m-0'>Username <i className="icofont icofont-businessman"></i></Label>
+                <Input className='form-control' type='text' placeholder='User_name' onChange={(e) => setUsername(e.target.value)} value={username} />
+              </FormGroup>
+              <FormGroup className='position-relative'>
+                <Label className='col-form-label m-0'>Password <i className="icofont icofont-ui-fire-wall"></i></Label>
+                <div className='position-relative'>
+                  <Input className='form-control' type={togglePassword ? 'text' : 'password'} name='login[password]' required placeholder='*********' onChange={(e) => setPassword(e.target.value)} value={password} />
+                  <div className='show-hide' onClick={() => setTogglePassword(!togglePassword)}>
+                    <span className={togglePassword ? '' : 'show'}></span>
                   </div>
-                </FormGroup>
-                <FormGroup>
-                  <Btn attrBtn={{ className: 'd-block btn-pill btn-air-primary w-100 mt-2 btn-primary-gradien', color: 'primary', type: 'submit' }}>Sign in</Btn>
-                </FormGroup>
-              </Form>
-            </div>
+                </div>
+              </FormGroup>
+              <FormGroup>
+                <Btn attrBtn={{ className: 'd-block btn-pill btn-air-primary w-100 mt-2 btn-primary-gradien', color: 'primary', type: 'submit' }}>Sign in</Btn>
+              </FormGroup>
+            </Form>
           </div>
         </div>
+      </div>
     </Fragment>
   );
 };
